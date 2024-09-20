ba482561c56256ecd52b6ac3d96bc971c1b58ab7
unknown <GastonPuyares>
2024-09-20 19:13:31 -0300

    main

---
+++ src/Library/Gun.cs
@@ -0,0 +1,13 @@
+﻿namespace Program;
+
+public class Gun
+{
+    public string Nombre { get; set; }
+    public int Ataque { get; set; }
+
+    public Gun(string nombre, int ataque)
+    {
+        this.Nombre = nombre;
+        this.Ataque = ataque;
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Program;
+
+class Program
+{
+    static void Main()
+    {
+        Gun salamalecons = new Gun("Salamalecons", 3);
+        Escudo armor = new Escudo("Armor", 2);
+
+        Dwarf brokk = new Dwarf("Brokk", salamalecons, 75, armor);
+
+        Gun karambit = new Gun("Salamalecons", 4);
+        Escudo shield = new Escudo("Shield", 1);
+
+        Dwarf minidwarf = new Dwarf("Kokemon", karambit, 50, shield);
+
+        brokk.Atacar(minidwarf);
+        minidwarf.Curar();
+
+    }
+}
