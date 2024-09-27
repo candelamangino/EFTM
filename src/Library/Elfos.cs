fcc54c63ea26b3bd47c912ae195c9d5afc15b2a1
candela <candelamangino@gmail.com>
2024-09-27 19:28:41 -0300

    elfos con Icharacter

--- src/Library/Elfos.cs
+++ src/Library/Elfos.cs
@@ -1,21 +1,50 @@
 ﻿namespace Library;
 
-public class Elfos
+public class Elfos : ICharacter
 {
     public string Name { get; set; }
-    public int Life { get; set; }
+    public int Health { get; set; }
+    public int HealthMaxima { get; private set; }
+    
     public Gun gun;
     public Club club;
 
-    public Elfos(string name, int life, Gun gun_, Club club_)
+    public Elfos(string name, int health, Gun gun_, Club club_)
     {
-        Name = name;
-        Life = life;
-        gun = gun_;
-        club = club_;
+        this.Name = name;
+        this.Health = health;
+        this.gun = gun_;
+        this.club = club_;
+        this.HealthMaxima = health;
+    }
+    public void Attack(ICharacter objective)
+    {
+        int damage = this.ObtainAttackValue() - objective.ObtainDefenseValue();
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+    
+        objective.Health -= damage;
+    
+        if (objective.Health < 0)
+        {
+            objective.Health = 0;
+        }
+            
+        Console.WriteLine(this.Name + $" ataca a {Name}, causando " + damage + " de daño. XXX tiene ahora " + objective.Health + " de vida.");
+
     }
 
-    public int GetAttackVakue()
+    public void Heal()
+    {
+        int Health = this.HealthMaxima;
+        Console.WriteLine($"{this.Name} se curó y ahora tiene {Health} de vida");
+    }
+
+
+
+    public int ObtainAttackValue()
     {
         int attackValue = 10;
         if (gun != null)
@@ -31,7 +60,7 @@ public class Elfos
         return attackValue;
     }
 
-    public int GetDefenseValue()
+    public int ObtainDefenseValue()
     {
         int defenseValue = 5;
         if (gun != null)
@@ -69,21 +98,21 @@ public class Elfos
     {
         if (contrincante != null)
         {
-            int attack = GetAttackVakue();
-            int defenseContrincante = contrincante.GetDefenseValue();
+            int attack = ObtainAttackValue();
+            int defenseContrincante = contrincante.ObtainDefenseValue();
 
             int daño = attack - defenseContrincante;
 
-            contrincante.Life -= daño;
+            contrincante.Health -= daño;
             Console.WriteLine(
-                $"{Name} ataca a {contrincante.Name}, y le hace {daño} daño y queda con {contrincante.Life} vidas");
+                $"{Name} ataca a {contrincante.Name}, y le hace {daño} daño y queda con {contrincante.Health} vidas");
         }
     }
 
     public void Heal(int cantidad)
         {
-            Life += cantidad;
-            Console.WriteLine($"{Name} se curo y ahora tiene {Life} vidas");
+            Health += cantidad;
+            Console.WriteLine($"{Name} se curo y ahora tiene {Health} vidas");
         }
     
 }
